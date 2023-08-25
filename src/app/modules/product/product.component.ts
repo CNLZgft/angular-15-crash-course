463a1339bd746c1ee9b4f335c3fc050693498f92
CNLZgft <cristian.lopez@gft.com>
2023-08-25 10:28:02 +0200

    update UI based on the response from the API

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,20 +1,27 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ProductResponse } from 'src/app/models/product.interface';
+import {
+  ProductInterface,
+  ProductResponse,
+} from 'src/app/models/product.interface';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css'],
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  products: ProductInterface[] = [];
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
-      next: (value: ProductResponse) => {
-        console.log(value);
+      next: (productResponse: ProductResponse) => {
+        this.products = productResponse.products;
+
+        console.log(productResponse);
       },
       error: (error: HttpErrorResponse) => {
         console.log(error);
